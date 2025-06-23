4fd589a793a9d1b56244e7d3137c9b513392af2c
Axel Pincheira <teck007@gmail.com>
2025-06-22 21:33:36 -0400

    add api token

--- app.py
+++ app.py
@@ -3,11 +3,28 @@ from src.chat_openai import shorten
 from src.url_analize import url_content
 from flask_cors import CORS
 from src.database import save_url, get_url, find_url
+import os
+from dotenv import load_dotenv
+from functools import wraps
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+# Cargar variables de entorno
+API_TOKEN = os.getenv('API_TOKEN')
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token or token != f"Bearer {API_TOKEN}":
+            return jsonify({'error': 'No autorizado'}), 401
+        return f(*args, **kwargs)
+    return decorated
+
 @app.route('/api/shorten',methods=['POST'])
+@token_required
 def short():
     data = request.get_json()
     url = data['url']
@@ -20,6 +37,7 @@ def short():
     return jsonify({'status': 'shorted','result': short_url})
 
 @app.route('/<short_url>', methods=['GET'])
+@token_required
 def redirect_short(short_url):
     original_url = get_url(short_url)
     if original_url:
